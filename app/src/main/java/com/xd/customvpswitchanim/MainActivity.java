d33bfd73a1f71b66c5cf606cf71022e4d33f0297
He Wei <xdhhhwww@foxmail.com>
2016-03-16 23:17:16 +0800

    t

--- app/src/main/java/com/xd/customvpswitchanim/MainActivity.java
+++ app/src/main/java/com/xd/customvpswitchanim/MainActivity.java
@@ -1,13 +1,27 @@
 package com.xd.customvpswitchanim;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private int[] mIds = {R.drawable.guide_image1, R.drawable.guide_image2
+            , R.drawable.guide_image3};
+
+    private List<ImageView> mImageViews = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.vp);
     }
 }
