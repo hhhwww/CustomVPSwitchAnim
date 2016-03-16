869c8d5a8cc141aee8521884fbc049df81507d11
He Wei <xdhhhwww@foxmail.com>
2016-03-16 23:27:42 +0800

    搭建好了基础的框架

--- app/src/main/java/com/xd/customvpswitchanim/MainActivity.java
+++ app/src/main/java/com/xd/customvpswitchanim/MainActivity.java
@@ -1,8 +1,11 @@
 package com.xd.customvpswitchanim;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -11,10 +14,11 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
+    private List<ImageView> mImageViews = new ArrayList<>();
+    private CustomViewPager mCustomViewPager;
+
     private int[] mIds = {R.drawable.guide_image1, R.drawable.guide_image2
             , R.drawable.guide_image3};
-
-    private List<ImageView> mImageViews = new ArrayList<>();
 
 
     @Override
@@ -23,5 +27,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mViewPager = (ViewPager) findViewById(R.id.vp);
+        initDatas();
+
+    }
+
+    private void initDatas() {
+        for (int i = 0; i < mIds.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(mIds[i]);
+            mImageViews.add(imageView);
+        }
+        mCustomViewPager = new CustomViewPager();
+        mViewPager.setAdapter(mCustomViewPager);
+    }
+
+    class CustomViewPager extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return mImageViews.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = mImageViews.get(position);
+            container.addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(mImageViews.get(position));
+        }
     }
 }
